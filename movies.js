5d7d6d3f4b803c4c8941d2283c90def96c12fffa
Amber C <amberpomar@gmail.com>
2023-02-23 23:17:27 +0100

    Movies

---
+++ movies.js
@@ -0,0 +1,63 @@
+class Movie {
+
+    //constructor
+
+    constructor(title, releaseYear, nationality, genre, photo) {
+        this.title = title
+        this.releaseYear = releaseYear
+        this.nationality = nationality
+        this.genre = genre
+        this.actors = []
+        this.writer
+        this.director
+        this.photo = photo
+    }
+
+    // methods
+}
+
+let fMRF = new Movie('Fantastic Mr Fox', 2009, 'American', 'Animation', 'https://www.themoviedb.org/t/p/original/pYbIT04CMXAbVEPj9mhFzcM73XS.jpg')
+let ssT = new Movie('Starship Troopers', 1997, 'American', 'Comedy', 'https://image.tmdb.org/t/p/original/s7kzkCz6KfsjQz1yqQQOjgqHuWZ.jpg')
+
+let moviesList = [fMRF, ssT]
+
+jQuery(() => {
+
+    $("#form-submit").on('click', () => {
+        let title = $("#title").val(),
+            releaseYear = $("#releaseYear").val(),
+            nationality = $("#nationality").val(),
+            genre = $("#genre").val(),
+            photo = $("#photoURL").val()
+
+        moviesList.push(new Movie(title,releaseYear,nationality,genre,photo))
+        console.log(moviesList);
+    })
+
+    $("#nav-movies").on('click', () => {
+        let listedMovies = ""
+        for (const movie in moviesList) {
+            listedMovies += `<div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="${movie.photo}" class="card-img-top img-fluid" alt="${movie.title}">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${movie.title}</h5>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Release Year -- ${movie.releaseYear}</li>
+                                        <li class="list-group-item">Nationality -- ${movie.nationality}</li>
+                                        <li class="list-group-item">Genre -- ${movie.releaseYear}</li>
+                                        <li class="list-group-item">Actors -- ${movie.actors}</li>
+                                        <li class="list-group-item">Release Year -- ${movie.releaseYear}</li>
+                                        <li class="list-group-item">Writer -- ${movie.writer}</li>
+                                        <li class="list-group-item">Director -- ${movie.director}</li>
+                                    </ul>
+                                    <div class="card-body">
+                                    <a href="#" class="card-link">More info</a>
+                                    </div>
+                                </div>
+                            </div>`}
+                            console.log(listedMovies);
+        $("#movie-area").append(listedMovies)
+               
+    })
+})
